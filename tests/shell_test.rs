4a9f4fd35f18c4117e6b42fbd61e078127525489
Jordi Carrillo Bosch <jordilin@gmail.com>
2023-08-26 23:06:31 -0700

    test shell - use path relative to cargo manifest
    Signed-off-by: Jordi Carrillo Bosch <jordilin@gmail.com>

--- tests/shell_test.rs
+++ tests/shell_test.rs
@@ -20,7 +20,10 @@ fn test_execute_shell_cmd_with_error_cmd_does_not_exist() {
 #[test]
 fn test_execute_shell_cmd_with_error_cmd_fails_to_stderr() {
     let runner = gr::shell::Shell;
-    let cmd = ["sh", "-c", "./tests/fail_msg_to_stderr.sh"];
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let err_cmd = format!("{}/tests/fail_msg_to_stderr.sh", manifest_dir);
+    let cmd = ["sh", "-c", &err_cmd];
+    // let cmd = ["sh", "-c", "./tests/fail_msg_to_stderr.sh"];
     let err = runner.run(cmd).unwrap_err();
     assert_eq!(err.to_string(), "This is a failure message");
 }
