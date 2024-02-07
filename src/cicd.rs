5a63c373ef02401b79b982ead3e069013cfb5695
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-02-06 21:31:29 -0800

    cicd list num pages test

--- src/cicd.rs
+++ src/cicd.rs
@@ -1,4 +1,4 @@
-use crate::api_traits::{ApiOperation, Cicd};
+use crate::api_traits::{ApiOperation, Cicd, ListPages};
 use crate::cli::PipelineOptions;
 use crate::config::Config;
 use crate::remote::PipelineBodyArgs;
@@ -29,18 +29,7 @@ pub fn execute(
         PipelineOptions::List(cli_args) => {
             if cli_args.num_pages {
                 let remote = remote::get_list_pages(domain, path, config, cli_args.refresh_cache)?;
-                match remote.num_pages(&ApiOperation::Pipeline) {
-                    Ok(Some(pages)) => {
-                        println!("Number of pages: {}", pages);
-                    }
-                    Ok(None) => {
-                        println!("Number of pages not available.");
-                    }
-                    Err(e) => {
-                        return Err(e);
-                    }
-                };
-                return Ok(());
+                return execute_list_pages(remote, std::io::stdout());
             }
             let remote = remote::get_cicd(domain, path, config, cli_args.refresh_cache)?;
             let from_to_args = remote::validate_from_to_page(&cli_args)?;
@@ -50,6 +39,19 @@ pub fn execute(
             list_pipelines(remote, body_args, std::io::stdout())
         }
     }
+}
+
+fn execute_list_pages<W: Write>(remote: Arc<dyn ListPages>, mut writer: W) -> Result<()> {
+    match remote.num_pages(&ApiOperation::Pipeline) {
+        Ok(Some(pages)) => writer.write_all(format!("{pages}\n", pages = pages).as_bytes())?,
+        Ok(None) => {
+            writer.write_all(b"Number of pages not available.\n")?;
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    Ok(())
 }
 
 fn list_pipelines<W: Write>(
@@ -77,20 +79,22 @@ mod test {
     use super::*;
 
     #[derive(Clone, Builder)]
-    struct PipelineList {
+    struct PipelineListMock {
         #[builder(default = "vec![]")]
         pipelines: Vec<Pipeline>,
         #[builder(default = "false")]
         error: bool,
+        #[builder(setter(into, strip_option), default)]
+        num_pages: Option<u32>,
     }
 
-    impl PipelineList {
-        pub fn builder() -> PipelineListBuilder {
-            PipelineListBuilder::default()
+    impl PipelineListMock {
+        pub fn builder() -> PipelineListMockBuilder {
+            PipelineListMockBuilder::default()
         }
     }
 
-    impl Cicd for PipelineList {
+    impl Cicd for PipelineListMock {
         fn list(&self, _args: PipelineBodyArgs) -> Result<Vec<Pipeline>> {
             if self.error {
                 return Err(error::gen("Error"));
@@ -105,9 +109,18 @@ mod test {
         }
     }
 
+    impl ListPages for PipelineListMock {
+        fn num_pages(&self, _op: &ApiOperation) -> Result<Option<u32>> {
+            if self.error {
+                return Err(error::gen("Error"));
+            }
+            return Ok(self.num_pages);
+        }
+    }
+
     #[test]
     fn test_list_pipelines() {
-        let pp_remote = PipelineList::builder()
+        let pp_remote = PipelineListMock::builder()
             .pipelines(vec![
                 Pipeline::builder()
                     .status("success".to_string())
@@ -143,7 +156,7 @@ mod test {
 
     #[test]
     fn test_list_pipelines_empty() {
-        let pp_remote = PipelineListBuilder::default().build().unwrap();
+        let pp_remote = PipelineListMock::builder().build().unwrap();
         let mut buf = Vec::new();
         let args = PipelineBodyArgs::builder()
             .from_to_page(None)
@@ -155,12 +168,41 @@ mod test {
 
     #[test]
     fn test_list_pipelines_error() {
-        let pp_remote = PipelineListBuilder::default().error(true).build().unwrap();
+        let pp_remote = PipelineListMock::builder().error(true).build().unwrap();
         let mut buf = Vec::new();
         let args = PipelineBodyArgs::builder()
             .from_to_page(None)
             .build()
             .unwrap();
         assert!(list_pipelines(Arc::new(pp_remote), args, &mut buf).is_err());
+    }
+
+    #[test]
+    fn test_list_number_of_pages() {
+        let pp_remote = PipelineListMock::builder()
+            .num_pages(3 as u32)
+            .build()
+            .unwrap();
+        let mut buf = Vec::new();
+        execute_list_pages(Arc::new(pp_remote), &mut buf).unwrap();
+        assert_eq!("3\n", String::from_utf8(buf).unwrap(),)
+    }
+
+    #[test]
+    fn test_no_pages_available() {
+        let pp_remote = PipelineListMock::builder().build().unwrap();
+        let mut buf = Vec::new();
+        execute_list_pages(Arc::new(pp_remote), &mut buf).unwrap();
+        assert_eq!(
+            "Number of pages not available.\n",
+            String::from_utf8(buf).unwrap(),
+        )
+    }
+
+    #[test]
+    fn test_number_of_pages_error() {
+        let pp_remote = PipelineListMock::builder().error(true).build().unwrap();
+        let mut buf = Vec::new();
+        assert!(execute_list_pages(Arc::new(pp_remote), &mut buf).is_err());
     }
 }
