aaa862f1874778498c02a4ea5970b10040e31496
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-02-10 10:34:15 -0800

    Extract common utilities

--- contracts/verify_gitlab.py
+++ contracts/verify_gitlab.py
@@ -3,6 +3,8 @@ import json
 import os
 import argparse
 
+from validation import validate_responses
+
 PRIVATE_TOKEN = os.environ["GITLAB_TOKEN"]
 
 parser = argparse.ArgumentParser()
@@ -111,63 +113,11 @@ def get_contract_json(name):
         return data_json
 
 
-def _verify_all_keys_exist(expected, actual):
-    for key in expected:
-        if key not in actual:
-            print("Expected JSON key [{}] not found in upstream".format(key))
-            return False
-        if type(expected[key]) == dict:
-            # API responses checked are not more than one level deep
-            if not _verify_all_keys_exist(expected[key], actual[key]):
-                return False
-    return True
-
-
-def _verify_types_of_values(expected, actual):
-    for key in expected:
-        if type(expected[key]) != type(actual[key]):
-            print(
-                "Type mismatch for key [{}]: expected [{}] but got [{}]".format(
-                    key, type(expected[key]), type(actual[key])
-                )
-            )
-            return False
-        if type(expected[key]) == dict:
-            # API responses checked are not more than one level deep
-            if not _verify_types_of_values(expected[key], actual[key]):
-                return False
-    return True
-
-
-def verify_all(expected, actual):
-    if not _verify_all_keys_exist(expected, actual):
-        return False
-    if not _verify_types_of_values(expected, actual):
-        return False
-    return True
-
-
 class TestAPI:
     def __init__(self, callback, msg, *expected):
         self.callback = callback
         self.msg = msg
         self.expected = expected
-
-
-def validate_responses(testcases):
-    for testcase in testcases:
-        actual = testcase.callback()
-        print("{}... ".format(testcase.msg), end="")
-        verifications = []
-        if type(actual) == tuple:
-            verifications = zip(testcase.expected, actual)
-        else:
-            verifications = zip(testcase.expected, [actual])
-        for expected, actual in verifications:
-            if not verify_all(expected, actual):
-                return False
-        print("OK")
-    return True
 
 
 if __name__ == "__main__":
