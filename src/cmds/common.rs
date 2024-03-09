7be63c69b1cdeea16de585e4893c48e638fa62b3
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-03-09 12:29:08 -0800

    docker reuse common num_pages

--- src/cmds/common.rs
+++ src/cmds/common.rs
@@ -8,20 +8,22 @@ use crate::api_traits::Deploy;
 macro_rules! query_pages {
     ($func_name:ident, $trait_name:ident) => {
         pub fn $func_name<W: Write>(remote: Arc<dyn $trait_name>, mut writer: W) -> Result<()> {
-            match remote.num_pages() {
-                Ok(Some(pages)) => {
-                    writer.write_all(format!("{pages}\n", pages = pages).as_bytes())?
-                }
-                Ok(None) => {
-                    writer.write_all(b"Number of pages not available.\n")?;
-                }
-                Err(e) => {
-                    return Err(e);
-                }
-            };
-            Ok(())
+            process_num_pages(remote.num_pages(), &mut writer)
         }
     };
+}
+
+pub fn process_num_pages<W: Write>(num_pages: Result<Option<u32>>, mut writer: W) -> Result<()> {
+    match num_pages {
+        Ok(Some(pages)) => writer.write_all(format!("{pages}\n", pages = pages).as_bytes())?,
+        Ok(None) => {
+            writer.write_all(b"Number of pages not available.\n")?;
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    Ok(())
 }
 
 query_pages!(num_release_pages, Deploy);
