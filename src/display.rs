2f39fa040f4021b6904d118bc9856aff2d090997
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-03-19 22:33:58 -0700

    Remove impl Display for Format

--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,5 @@
 use crate::Result;
-use std::{collections::HashMap, fmt::Display, io::Write};
+use std::{collections::HashMap, io::Write};
 
 #[derive(Clone, Debug, Default)]
 pub enum Format {
@@ -7,16 +7,6 @@ pub enum Format {
     JSON,
     #[default]
     PIPE,
-}
-
-impl Display for Format {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Format::CSV => write!(f, ","),
-            Format::PIPE => write!(f, "|"),
-            Format::JSON => write!(f, ""),
-        }
-    }
 }
 
 impl From<&Format> for u8 {
