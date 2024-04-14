ae0b407d688ec172f811aa6428ba0d7a4dc52649
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-04-13 22:45:38 -0700

    Use flake crane for caching

--- flake.nix
+++ flake.nix
@@ -5,20 +5,42 @@
     nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        cargoManifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = pkgs.rust-bin.stable.latest.minimal;
-          rustc = pkgs.rust-bin.stable.latest.minimal;
+
+        inherit (pkgs) lib;
+        rust = pkgs.rust-bin.stable.latest;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust.default;
+
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
         };
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.openssl
+          ];
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        gr = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+
         gitcleanbranches = pkgs.writeScriptBin "git-clean-branches" ''
           # ref: https://gist.github.com/jordilin/08b6fed14b358dcbdf40c485c4daa730
           git branch -r --merged |
@@ -39,10 +61,18 @@
       in
       with pkgs;
       {
+        checks = {
+          inherit gr;
+        };
+
+        packages = {
+          default = gr;
+          inherit gr;
+        };
+
         devShell = mkShell {
+          inputsFrom = [ gr ];
           buildInputs = [
-            openssl
-            pkg-config
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-src" "llvm-tools-preview" ];
             })
@@ -80,19 +110,6 @@
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
-
-       packages = {
-        default = rustPlatform.buildRustPackage {
-          pname = cargoManifest.name;
-          version = cargoManifest.version;
-          src = ./.;
-          nativeBuildInputs = [ pkg-config openssl ];
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        };
-      };
     }
   );
 }
