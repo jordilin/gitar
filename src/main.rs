da35012572a80553115bfd73f8c21e8775acb367
Jordi Carrillo Bosch <jordilin@gmail.com>
2024-05-21 22:49:11 -0700

    main: better error handling, error msgs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use env_logger::Env;
 use gr::{
     cli::{parse_cli, trending::TrendingOptions, CliOptions},
     cmds::{self, browse, cicd, docker, merge_request, project},
-    error, git, init,
+    error::GRError,
+    git, init,
     io::CmdInfo,
     remote::get_domain_path,
     shell::Shell,
@@ -22,17 +23,35 @@ fn get_config_domain_path(
     let (domain, path) = if cli_args.repo.is_some() {
         get_domain_path(cli_args.repo.as_ref().unwrap())
     } else if requires_local_repo {
-        let CmdInfo::RemoteUrl { domain, path } = git::remote_url(&Shell)? else {
-            return Err(error::gen("No remote url found. Please set a remote url."));
-        };
-        (domain, path)
+        match git::remote_url(&Shell) {
+            Ok(CmdInfo::RemoteUrl { domain, path }) => (domain, path),
+            Err(err) => {
+                return Err(GRError::GitRemoteUrlNotFound(format!(
+                    "{}: {}",
+                    err,
+                    "Unable to get the remote url.".to_string()
+                ))
+                .into())
+            }
+            _ => {
+                return Err(GRError::ApplicationError(
+                    "Could not get remote url during startup. \
+                    main::get_config_domain_path - Please open a bug to \
+                    https://github.com/jordilin/gitar"
+                        .to_string(),
+                )
+                .into())
+            }
+        }
     } else if cli_args.domain.is_some() {
         (
             cli_args.domain.as_ref().unwrap().to_string(),
             "".to_string(),
         )
     } else {
-        return Err(error::gen("No domain found. Please set a domain."));
+        return Err(
+            GRError::DomainOrRepoExpected("Missing repository information".to_string()).into(),
+        );
     };
     if read_config {
         let f = File::open(config_file).expect("Unable to open config file");
@@ -56,6 +75,37 @@ fn main() -> Result<()> {
         let env = Env::default().default_filter_or("info");
         env_logger::init_from_env(env);
     }
+    match handle_cli_options(cli_options, config_file, cli_args) {
+        Err(err) => match err.downcast_ref::<GRError>() {
+            Some(GRError::GitRemoteUrlNotFound(msg)) => {
+                eprintln!(
+                    "Please cd into a git repository or set --repo option - {}",
+                    msg
+                );
+                std::process::exit(1);
+            }
+            Some(GRError::DomainOrRepoExpected(msg)) => {
+                eprintln!(
+                    "Please set a domain with --domain flag or cd \
+                into a git repository or set --repo option - {}",
+                    msg
+                );
+                std::process::exit(1);
+            }
+            _ => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        },
+        Ok(_) => Ok(()),
+    }
+}
+
+fn handle_cli_options(
+    cli_options: CliOptions,
+    config_file: std::path::PathBuf,
+    cli_args: gr::cli::CliArgs,
+) -> Result<()> {
     match cli_options {
         CliOptions::MergeRequest(options) => {
             let (config, domain, path) =
